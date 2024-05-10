21cb790c56f2952acb5062c7295c637963714c73
amandaptrk <amandaprhub@gmail.com>
2024-05-10 14:29:56 +0700

    final update api testing

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,20 +1,11 @@
 import { test, expect } from '@playwright/test'
 
-
-test.describe.parallel("API Testing", () => {
+test.describe.parallel('Checkout Process', () => {
     const baseUrl = 'https://gorest.co.in/public'
     const token = 'access-token=0813b251f3ee401dfb3abc153c164d666baebab085c2efd192fa2e914159f9ad'
-
-    test('Simple API Test - Assert Response Status', async ({ request }) => {
-        const response = await request.get(`${baseUrl}/v2/users/6900806?${token}`)
-        expect(response.status()).toBe(200)
-
-        const responseBody = JSON.parse(await response.text())
-        console.log(responseBody);
-    })
-
+    
     test('POST Request - Create a new user', async ({ request }) => {
-        const response = await request.post(`${baseUrl}/v2/users/?access-token=0813b251f3ee401dfb3abc153c164d666baebab085c2efd192fa2e914159f9ad`, {
+        const response = await request.post(`${baseUrl}/v2/users/?${token}`, {
             data: {
                 name: 'Nana Komatsu',
                 email: 'komatsu_nana@blast.ex',
@@ -24,9 +15,33 @@ test.describe.parallel("API Testing", () => {
         })
         const responseBody = JSON.parse(await response.text())
         expect(response.status()).toBe(201)
-        expect(responseBody.name).toBe('Nana Komtsu')
+        expect(responseBody.name).toBe('Nana Komatsu')
         expect(responseBody.email).toBe('komatsu_nana@blast.ex')
         expect(responseBody.gender).toBe('female')
         expect(responseBody.status).toBe('active')
     })
-})
+
+    test('GET Request - Get user details', async ({ request }) => {
+        const response = await request.get(`${baseUrl}/v2/users/6900806?${token}`)
+        expect(response.status()).toBe(200)
+
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        console.log(responseBody);
+    })
+
+    test('PUT Request - Update user details', async ({ request }) => {
+        const response = await request.put(`${baseUrl}/v2/users/6900806?${token}`, {
+            data: {
+                name: 'Nana Ichinose',
+                status: 'inactive'
+            }
+        })
+        const responseBody = JSON.parse(await response.text())
+        expect(response.status()).toBe(200)
+        expect(responseBody.name).toBe('Nana Ichinose')
+        expect(responseBody.email).toBe('komatsu_nana@blast.ex')
+        expect(responseBody.gender).toBe('female')
+        expect(responseBody.status).toBe('inactive')
+    });
+});
